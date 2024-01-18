6c2a97d856d26d2d1a683b29009912015d2842bf
Eero <eero.iltanen@edu.omnia.fi>
2024-01-18 10:49:32 +0200

    initial

---
+++ simple_nav/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>
+        <?= $title ?? "Otsikko"; ?>
+    </title>
+</head>
+
+<body>
+
+    <header>Sivusto X</header>
+
+
+    <?php
+
+    function getLinkClass(string $page): string
+    {
+        if ($_SERVER['SCRIPT_NAME'] === $page) {
+            return 'class="active"';
+        }
+        return '';
+    }
+
+
+    ?>
+
+    <nav>
+        <label class="menu-toggle" for="menu-btn"><i class="fa-solid fa-bars fa-2x"></i></label>
+        <input class="menu-toggle" type="checkbox" id="menu-btn" />
+
+        <ul id="nav-menu">
+            <li><a href="/" <?= getLinkClass("/index.php"); ?>>Etusivu</a></li>
+            <li><a href="/info.php" <?= getLinkClass("/info.php"); ?>>Tietoa meistä</a></li>
+            <li><a href="/yhteydenotto.php" <?= getLinkClass("/yhteydenotto.php"); ?>>Yhteydenotto</a></li>
+            <li><a href="/blog.php" <?= getLinkClass("/blog.php"); ?>>Blogi</a></li>
+        </ul>
+
+    </nav>
---
+++ simple_nav/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$title = "Sivu X";
+include("header.php");
+
+?>
+
+
+
+<main>
+
+    <h2>Etusivu</h2>
+
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum."</p>
+
+
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum."</p>
+
+    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum."</p>
+
+
+</main>
+
+<?php include("footer.php"); ?>
