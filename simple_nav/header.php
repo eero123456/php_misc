a6531425db6f60c1951bda50db326123dacd2661
Eero <eero.iltanen@edu.omnia.fi>
2024-03-11 13:12:15 +0200

    login/register fix

--- simple_nav/header.php
+++ simple_nav/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -22,8 +22,8 @@
     <?php
 
     function getLinkClass(string $page): string
-    {
-        if ($_SERVER['SCRIPT_NAME'] === $page) {
+    {       
+        if (str_ends_with($_SERVER['SCRIPT_NAME'],$page)) {
             return 'class="active"';
         }
         return '';
@@ -37,10 +37,10 @@
         <input class="menu-toggle" type="checkbox" id="menu-btn" />
 
         <ul id="nav-menu">
-            <li><a href="/" <?= getLinkClass("/index.php"); ?>>Etusivu</a></li>
-            <li><a href="/info.php" <?= getLinkClass("/info.php"); ?>>Tietoa meistä</a></li>
-            <li><a href="/yhteydenotto.php" <?= getLinkClass("/yhteydenotto.php"); ?>>Yhteydenotto</a></li>
-            <li><a href="/blog.php" <?= getLinkClass("/blog.php"); ?>>Blogi</a></li>
+            <li><a href="index.php" <?= getLinkClass("index.php"); ?>>Etusivu</a></li>
+            <li><a href="info.php" <?= getLinkClass("info.php"); ?>>Tietoa meistä</a></li>
+            <li><a href="yhteydenotto.php" <?= getLinkClass("yhteydenotto.php"); ?>>Yhteydenotto</a></li>
+            <li><a href="blog.php" <?= getLinkClass("blog.php"); ?>>Blogi</a></li>
         </ul>
 
     </nav>
